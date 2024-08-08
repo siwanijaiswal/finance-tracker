1d737e8cd71868a39b84d9688b888ea8bca4426c
Siwani jaiswal <siwani.jce20@sot.pdpu.ac.in>
2024-08-08 15:39:44 +0530

    fetch transaction document from a collection in firebase

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,15 +3,17 @@ import Cards from '../../components/Cards/Cards';
 import moment from 'moment';
 import AddExpense from '../../components/Modals/AddExpense';
 import AddIncome from '../../components/Modals/AddIncome';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../utils/firebase/firebase';
 import { toast } from 'react-toastify';
 import { db } from '../../utils/firebase/firebase';
+import { getIdToken } from 'firebase/auth/web-extension';
 
 const Dashboard = () => {
   const [user] = useAuthState(auth);
   const [transaction, setTransaction] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
 
@@ -56,23 +58,51 @@ const Dashboard = () => {
     }
   }
 
+  useEffect(() => {
+    //get all docs from firebase collection
+    fetchTransaction();
+  }, []);
+
+  const fetchTransaction = async () => {
+    setLoading(true);
+    if (user) {
+      const q = query(collection(db, `users/${user.uid}/transactions`));
+      const querySnapshot = await getDocs(q);
+      let transactionsArray = [];
+      querySnapshot.forEach((doc) => {
+        //doc.data() is never undefined for query doc snapshot
+        transactionsArray.push(doc.data());
+      });
+      setTransaction(transactionsArray);
+      console.log('Transaction array is', transactionsArray);
+      +toast.success('Transaction fetched');
+    }
+    setLoading(false);
+  };
+
   return (
     <div>
-      <Cards
-        showExpenseModel={showExpenseModel}
-        showIncomeModel={showIncomeModel}
-      />
-      <AddIncome
-        isIncomeModalVisible={isIncomeModalVisible}
-        handleIncomeCancel={handleIncomeCancel}
-        onFinish={onFinish}
-      />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <Cards
+            showExpenseModel={showExpenseModel}
+            showIncomeModel={showIncomeModel}
+          />
+          <AddIncome
+            isIncomeModalVisible={isIncomeModalVisible}
+            handleIncomeCancel={handleIncomeCancel}
+            onFinish={onFinish}
+          />
 
-      <AddExpense
-        isExpenseModalVisible={isExpenseModalVisible}
-        handleExpenseCancel={handleExpenseCancel}
-        onFinish={onFinish}
-      />
+          <AddExpense
+            isExpenseModalVisible={isExpenseModalVisible}
+            handleExpenseCancel={handleExpenseCancel}
+            onFinish={onFinish}
+          />
+        </div>
+      )}
     </div>
   );
 };
